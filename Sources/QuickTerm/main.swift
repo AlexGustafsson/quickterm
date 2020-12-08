c8126f8e8f31604be4aa3c19074fd66423cfe95a
Alex Gustafsson <alex-gustafsson@live.se>
2020-12-08 14:49:12 +0100

    Hide the application from the dock

--- Sources/QuickTerm/main.swift
+++ Sources/QuickTerm/main.swift
@@ -5,6 +5,8 @@ let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "main")
 
 logger.log("Initiating application")
 let app = NSApplication.shared
+// Hide the application from the dock
+app.setActivationPolicy(.accessory)
 let delegate = AppDelegate()
 app.delegate = delegate
 logger.info("Starting application")
