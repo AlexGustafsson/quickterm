829e668dbea3b3e620e72b1fe041691be0dcaf2c
Alex Gustafsson <alex-gustafsson@live.se>
2020-12-19 12:39:42 +0100

    Improve look of input view

--- Sources/QuickTerm/Views/InputView.swift
+++ Sources/QuickTerm/Views/InputView.swift
@@ -8,16 +8,18 @@ struct InputView: View {
   @State var command: String = ""
 
   var body: some View {
-    VStack(alignment: .leading) {
+    VStack(alignment: .center) {
       HStack(alignment: .center) {
-        Text(">").font(.custom("Fira Mono", size: 16)).padding()
+        Text(">").font(.custom("Fira Mono", size: 22)).foregroundColor(Color.gray)
         TextField("Enter command", text: $command, onEditingChanged: onEditingChanged(_:), onCommit: onCommit)
-          .font(.custom("Fira Mono", size: 16))
+          .font(.custom("Fira Mono", size: 22))
           .background(Color.clear)
           .textFieldStyle(PlainTextFieldStyle())
           .focusable()
           .introspectTextField {textField in textField.becomeFirstResponder()}
       }
+      .padding(.init(top: 10, leading: 15, bottom: 10, trailing: 15))
+      .frame(maxWidth: 680)
       .overlay(
         RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 1)
       )
@@ -28,7 +30,7 @@ struct InputView: View {
           cornerRadius: 10
         )
       )
-      .shadow(color: Color.black.opacity(0.8), radius: 15, x: 0, y: 25)
+      .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 25)
     }
     .padding(100)
   }
