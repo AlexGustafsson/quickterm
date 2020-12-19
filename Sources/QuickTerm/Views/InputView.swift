bf1d61f494c0575c6f7f306da6f04feaf884fcdb
Alex Gustafsson <alex-gustafsson@live.se>
2020-12-19 14:08:48 +0100

    Make input view less transparent

--- Sources/QuickTerm/Views/InputView.swift
+++ Sources/QuickTerm/Views/InputView.swift
@@ -10,7 +10,7 @@ struct InputView: View {
   var body: some View {
     VStack(alignment: .center) {
       HStack(alignment: .center) {
-        Text(">").font(.custom("Fira Mono", size: 22)).foregroundColor(Color(hex: 0x6C6C6C))
+        Text(">").font(.custom("Fira Mono", size: 22)).foregroundColor(Color.black.opacity(0.5))
         TextField("Enter command", text: $command, onEditingChanged: onEditingChanged(_:), onCommit: onCommit)
           .font(.custom("Fira Mono", size: 22))
           .background(Color.clear)
@@ -21,11 +21,11 @@ struct InputView: View {
       .padding(.init(top: 10, leading: 15, bottom: 10, trailing: 15))
       .frame(maxWidth: 680)
       .overlay(
-        RoundedRectangle(cornerRadius: 10).stroke(Color(hex: 0xD2D2D2, alpha: 0.9), lineWidth: 1)
+        RoundedRectangle(cornerRadius: 10).stroke(Color.black.opacity(0.1), lineWidth: 1)
       )
       .background(
         VisualEffectView(
-          material: NSVisualEffectView.Material.fullScreenUI,
+          material: NSVisualEffectView.Material.popover,
           blendingMode: NSVisualEffectView.BlendingMode.behindWindow,
           cornerRadius: 10
         )
