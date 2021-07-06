b05f90500235f0fa8eecd11d4d04e0c434ec4991
Alex Gustafsson <alex-gustafsson@live.se>
2021-07-06 09:18:54 +0200

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     .executable(name: "QuickTermBroker", targets: ["QuickTermBroker"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
+    .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.3")),
     .package(url: "https://github.com/soffes/HotKey", from: "0.1.3"),
-    .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.3"),
+    .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
   ],
   targets: [
     .target(
